77fb9baf171a32d3fa207680a3c8f7e99989de3c
Merve Helvaci <mervehelvacii45@gmail.com>
2022-12-13 15:16:53 +0300

    tableRow da column kısmı düzenlenerek tablonun verilerinin görüntülenmesi sağlandı.

--- src/TableCompanent.js
+++ src/TableCompanent.js
@@ -23,11 +23,11 @@ const TableHeadItem = ({ item }) => <th>{item.heading}</th>;
 const TableRow = ({ item, column }) => (
   <tr>
     {column.map((columnItem, index) => {
-      if (columnItem.value.includes(".")) {
-        const itemSplit = columnItem.value.split(".");
+      if (columnItem.value.includes('.')) {
+        const itemSplit = columnItem.value.split('.');
         return <td>{item[itemSplit[0]][itemSplit[1]]}</td>;
       }
-      return <td>{item["${columnItem.value}"]}</td>;
+      return <td>{item[columnItem.value]}</td>;
     })}
   </tr>
 );

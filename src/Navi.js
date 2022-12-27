9cd69a3b116422b10aa52c2afb0ab64b0440c85a
Merve Helvaci <mervehelvacii45@gmail.com>
2022-12-27 09:57:14 +0300

    navbar eklenerek dizaynı degiştirildi. cıkıs yapma eklendi

--- src/Navi.js
+++ src/Navi.js
@@ -1,69 +1,64 @@
-import React, { Component } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
+function Navi() {
+  const navigate = useNavigate();
 
-export default class Navi extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Navi Companent</h2>
-      </div>
-    );
-  }
-}
-/*
-import React, { useState } from "react";
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText,
-} from "reactstrap";
-
-function Navi(args) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
+  //Çıkış yap
+  const logOut = () => {
+    localStorage.setItem("name", "");
+    localStorage.setItem("username", "");
+    navigate("/");
+  };
 
   return (
-    <div>
-      <Navbar {...args}>
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
-      </Navbar>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light, justify-content-center">
+        <a className="navbar-brand" href="http://localhost:3000/home">
+          Ana Sayfa
+        </a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item, justify-content-end">
+              <FaUserCircle
+                className="d-flex justify-content-end "
+                size="50"
+                color="blue"
+                onClick={logOut}
+              />
+            </li>
+            <li className="nav-item dropdown,justify-content-end">
+              <a
+                className="nav-link dropdown-toggle"
+                href=""
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                User
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="/">
+                  Log Out
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
+    // <div class="d-flex justify-content-between">
+    //   <h2 class="align-items-center">Ana Sayfa</h2>
+    //   <FaUserCircle
+    //     class="d-flex justify-content-end "
+    //     size="50"
+    //     color="blue"
+    //     onClick={logOut}
+    //   />
+    //   <h4>Çıkış Yap</h4>
+    // </div>
   );
 }
-
 export default Navi;
-*/

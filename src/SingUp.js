515743fddb841fc4c90d5b379e55d074e4071e9b
Merve Helvaci <mervehelvacii45@gmail.com>
2022-12-27 14:48:40 +0300

    Navigate eklenerek kayıt olma sayfası hooks yapısına cevirildi. Form dizayn değiştirildi.

--- src/SingUp.js
+++ src/SingUp.js
@@ -1,128 +1,129 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
-//import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-export default class SingUp extends Component {
-  state = {
-    username: "",
+function SingUp() {
+  const [user, setUser] = useState({
     name: "",
+    username: "",
     email: "",
     phone: "",
     website: "",
-    hasError: false,
-    errorMessage: "",
-  };
-  onSingUp(e) {
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  //Kayıt işlemi
+  const onSingUp = (e) => {
     e.preventDefault();
-    console.log(this.state);
-    const model = this.state;
+    console.log(user);
+    const model = user;
 
+    //Inputlar boş mu kontrol eder
     if (
-      this.state.name === "" ||
-      this.state.username === "" ||
-      this.state.email === "" ||
-      this.state.phone === "" ||
-      this.state.website === ""
+      user.name === "" ||
+      user.username === "" ||
+      user.email === "" ||
+      user.phone === "" ||
+      user.website === ""
     ) {
-      this.setState({
-        hasError: true,
-        errorMessage: "Lütfen tüm alanları doldurunuz!",
-      });
+      setError("Lütfen tüm alanları doldurunuz!");
       return;
+    } else {
+      localStorage.setItem("name", user.name);
+      localStorage.setItem("username", user.username);
+      localStorage.setItem("email", user.email);
+      localStorage.setItem("phone", user.phone);
+      localStorage.setItem("website", user.website);
+
+      //Fetch ile kayıt yapılır
+      fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "POST",
+        body: JSON.stringify(model),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          if (result.id != null) {
+            alert("Kayıt Başarılı. Giriş sayfasına yönlendiriliyorsunuz.");
+            navigate("/");
+          } else {
+            alert("Lütfen bilgileri kontrol edin!");
+          }
+        })
+        .catch((err) => console.log(err));
     }
-    localStorage.setItem("name", this.state.name);
-    localStorage.setItem("username", this.state.username);
-    localStorage.setItem("email", this.state.email);
-    localStorage.setItem("phone", this.state.phone);
-    localStorage.setItem("website", this.state.website);
-
-    fetch("https://jsonplaceholder.typicode.com/users", {
-      method: "POST",
-      body: JSON.stringify(model),
-      headers: { "Content-type": "application/json; charset=UTF-8" },
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json))
-      .catch((err) => console.log(err));
-  }
-
-  onChangeHandler = (event) => {
-    let name = event.target.name;
-    let value = event.target.value;
-
-    this.setState({ [name]: value });
   };
 
-  render() {
-    //const Error={this.state.errorMessage};
-    let registerInfo = "Sing Up";
-    return (
-      <div>
-        {/* props veri aktarımı yapıldı */}
-        <h2>{registerInfo.title}</h2>
-
-        <Form onSubmit={this.onSingUp.bind(this)}>
-          <FormGroup>
-            <Label for="exampleName">Name</Label>
-            <Input
-              id="name"
-              name="name"
-              placeholder="Enter name"
-              type="text"
-              value={this.state.name}
-              onChange={this.onChangeHandler}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="exampleUserName">User Name</Label>
-            <Input
-              id="username"
-              name="username"
-              placeholder="Enter user name"
-              type="text"
-              value={this.state.username}
-              onChange={this.onChangeHandler}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="exampleEmail">Email</Label>
-            <Input
-              id="email"
-              name="email"
-              placeholder="Enter email"
-              type="email"
-              value={this.state.email}
-              onChange={this.onChangeHandler}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="examplePhone">Phone</Label>
-            <Input
-              id="phone"
-              name="phone"
-              placeholder="Enter phone"
-              type="text"
-              value={this.state.phone}
-              onChange={this.onChangeHandler}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="exampleWebsite">Website</Label>
-            <Input
-              id="website"
-              name="website"
-              placeholder="Enter website"
-              type="text"
-              value={this.state.website}
-              onChange={this.onChangeHandler}
-            />
-          </FormGroup>
-          <Button type="submit">Save</Button>
-          {/* Link ile diğer router a gidilir onClick={() => this.onSubmitHandler}*/}
-          {/* <Link to="Home">Submit</Link>  */}
-        </Form>
-        {this.state.hasError ? this.state.errorMessage : null}
+  return (
+    <div class="container ">
+      <div class="row height:100vh">
+        <div class="col-md-3 mx-auto">
+          <h2>Sing Up </h2>
+          <Form class="bg-blue" onSubmit={onSingUp}>
+            <FormGroup>
+              <Label for="exampleName">Name</Label>
+              <Input
+                id="name"
+                name="name"
+                placeholder="Enter name"
+                type="text"
+                value={user.name}
+                onChange={(e) => setUser({ ...user, name: e.target.value })}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleUserName">User Name</Label>
+              <Input
+                id="username"
+                name="username"
+                placeholder="Enter user name"
+                type="text"
+                value={user.username}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleEmail">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                placeholder="Enter email"
+                type="email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="examplePhone">Phone</Label>
+              <Input
+                id="phone"
+                name="phone"
+                placeholder="Enter phone"
+                type="text"
+                value={user.phone}
+                onChange={(e) => setUser({ ...user, phone: e.target.value })}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleWebsite">Website</Label>
+              <Input
+                id="website"
+                name="website"
+                placeholder="Enter website"
+                type="text"
+                value={user.website}
+                onChange={(e) => setUser({ ...user, website: e.target.value })}
+              />
+            </FormGroup>
+            <Button type="submit" class="btn btn-info btn-lg btn-block" onClick={onSingUp}>
+              Save
+            </Button>
+          </Form>
+          {error !== "" ? <div className="error text-danger">{error}</div> : ""}
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+export default SingUp;

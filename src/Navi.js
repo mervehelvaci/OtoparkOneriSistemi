dabce4d6d75205eec22b285f4c2bc2b66fdf246a
Merve Helvaci <mervehelvacii45@gmail.com>
2022-12-29 11:11:54 +0300

    Navbarda bölümlere ayırarak sona kaydırdık.

--- src/Navi.js
+++ src/Navi.js
@@ -19,25 +19,29 @@ function Navi() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <HiUserGroup size="70" color="#c349be" />
+        <HiUserGroup size="70" color="rgb(221 85 37)" />
         <Navbar.Brand href="/home">
           &nbsp;&nbsp;Kullanıcı Listeleme
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <BiUserCircle size="50" color="blue" />
-            <NavDropdown title="User" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4" onClick={logOut}>
-                Log Out
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="me-auto navEnd fullWith">
+            <div>
+              <Nav.Link href="/home">Home</Nav.Link>
+            </div>
+            <div className="navEnd">
+              <BiUserCircle size="50" color="rgb(221 85 37)" />
+              <NavDropdown title="User" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4" onClick={logOut}>
+                  Log Out
+                </NavDropdown.Item>
+              </NavDropdown>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
